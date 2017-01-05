db31cc7031adac79a0629c50cf42a30102271089
Ralf Gollmer <ralf.gollmer@uni-due.de>
2017-01-05 12:42:47 +0100

    tiny change

--- DDSIPinit.c
+++ DDSIPinit.c
@@ -240,8 +240,7 @@ DDSIP_InitCpxPara (void)
     DDSIP_param->cpxnodelim = i;
     DDSIP_param->cpxubnodelim = i;
     // Order
-    //status = CPXinfointparam (DDSIP_env, CPX_PARAM_MIPORDIND, &i, NULL, NULL);
-    //DDSIP_param->cpxorder = i;
+    DDSIP_param->cpxorder = 0;
 
     // save some parameters seperately
     for (i = 0; i < DDSIP_param->cpxno; i++)

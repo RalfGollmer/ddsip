f33d3c6b81835ddc1d730108dda35dcef00c5fe0
Ralf Gollmer <r.gollmer@gmx.de>
2022-03-25 14:34:44 +0100

    make redunancy check default

--- DDSIPread.c
+++ DDSIPread.c
@@ -1609,7 +1609,7 @@ DDSIP_ReadSpec ()
         DDSIP_param->numberScenReeval  = (int) floor (DDSIP_ReadDbl (specfile, "SCENRE", " NR OF SCENARIO REEVALUATIONS", (DDSIP_param->scenarios > 50?10:20), 1, -DDSIP_bigint, DDSIP_bigint) + 0.1);
         //DDSIP_param->numberScenReeval  = (int) floor (DDSIP_ReadDbl (specfile, "SCENRE", " NR OF SCENARIO REEVALUATIONS", (DDSIP_param->addBendersCuts?((DDSIP_param->cb && DDSIP_param->cb_depth > -1)?3:2):-1), 1, -DDSIP_bigint, (DDSIP_param->addBendersCuts?DDSIP_bigint:-1)) + 0.1);
         DDSIP_param->deactivate_cuts= (int) floor (DDSIP_ReadDbl (specfile, "DEACTI", " DEACTIVATE CUTS IN UB", 0., 1, 0., 1) + 0.1);
-        DDSIP_param->redundancyCheck = (int) floor (DDSIP_ReadDbl (specfile, "REDUND", " CHECK CUTS REDUNDANCY", DDSIP_param->deactivate_cuts, 1, 1., DDSIP_param->addBendersCuts ? 1.:0.) + 0.1);
+        DDSIP_param->redundancyCheck = (int) floor (DDSIP_ReadDbl (specfile, "REDUND", " CHECK CUTS REDUNDANCY", 1., 1, 0., DDSIP_param->addBendersCuts ? 1.:0.) + 0.1);
         if (DDSIP_param->redundancyCheck)
             DDSIP_param->deleteRedundantCuts = (int) floor (DDSIP_ReadDbl (specfile, "DELRED", " DELETE REDUNDANT CUTS", 1., 1, 0., 1.) + 0.1);
         else
